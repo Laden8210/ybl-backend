6d5fa6dbd0871097c1cbf7a5140a85d0771d8db6
Laden8210 <domingo.laden@gmail.com>
2025-11-18 19:31:13 +0800

    test

--- import-dump.php
+++ import-dump.php
@@ -9,7 +9,7 @@ $database = $_ENV['DB_DATABASE'];
 $username = $_ENV['DB_USERNAME'];
 $password = $_ENV['DB_PASSWORD'];
 
-$command = "mysql --host={$host} --user={$username} --password={$password} {$database} < mysql-schema.sql";
+$command = "mysql --host={$host} --user={$username} --password={$password} {$database} < database\schema\mysql-schema.sql";
 system($command);
 
 echo "Database imported successfully!\n";
